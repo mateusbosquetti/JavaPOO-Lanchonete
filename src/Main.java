0b7f0824dd793b934021c8cc86947d904ccaae25
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-08-02 21:51:46 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        GerenciadorDeCardapio gerenciadorDeCardapio = new GerenciadorDeCardapio();
         Scanner sc = new Scanner(System.in);
         int opcaoInicial, opcaoProduto;
 
@@ -36,6 +37,7 @@ public class Main {
 
                     switch (opcaoProduto) {
                         case 1:
+                            adicionarProduto(sc, gerenciadorDeCardapio);
                             // Metodo adicionar Produto
                             break;
                         case 2:
@@ -45,7 +47,8 @@ public class Main {
                             // Metodo editar Produto
                             break;
                         case 4:
-                            // Metodo listar Produto
+                            System.out.println(gerenciadorDeCardapio.listarProdutos());
+
                             break;
                         case 5:
                             //Voltar
@@ -71,5 +74,76 @@ public class Main {
             }
 
         } while (opcaoInicial != 5);
+        sc.close();
     }
+
+    //PRODUTOS
+    public static void adicionarProduto(Scanner sc, GerenciadorDeCardapio gerenciadorDeCardapio) {
+        int opcaoAdd;
+        System.out.println("Selecione a categoria do produto que deseja adicionar!\n" +
+                "___________________\n" +
+                "| Cod | Descricao |\n" +
+                "|  1  | Lanches   |\n" +
+                "|  2  | Bebidas   |\n" +
+                "|  3  | Outros    |\n" +
+                "|  4  | Combo     |\n" +
+                "|  5  | Voltar    |\n" +
+                "-------------------\n" +
+                "R: ");
+        opcaoAdd = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Digite a descrição do produto: ");
+        String descricao = sc.nextLine();
+
+        System.out.print("Digite o preço do produto: ");
+        double preco = sc.nextDouble();
+
+        switch (opcaoAdd) {
+            case 1:
+                System.out.print("Digite o peso do produto: ");
+                double peso = sc.nextDouble();
+                Lanche lanche = new Lanche(descricao, preco, peso);
+                gerenciadorDeCardapio.adicionarProduto(lanche);
+                System.out.println("Produto adicionado com sucesso!");
+                break;
+            case 2:
+                System.out.print("Digite o volume do produto: ");
+                double volume = sc.nextDouble();
+                Bebida bebida = new Bebida(descricao, preco, volume);
+                gerenciadorDeCardapio.adicionarProduto(bebida);
+                System.out.println("Produto adicionado com sucesso!");
+                break;
+            case 3:
+                System.out.print("Digite o peso do produto: ");
+                String tamanho = sc.nextLine();
+                Outro outro = new Outro(descricao, preco, tamanho);
+                gerenciadorDeCardapio.adicionarProduto(outro);
+                System.out.println("Produto adicionado com sucesso!");
+                break;
+            case 4:
+//                System.out.print("Digite o peso do produto: ");
+//                double peso = sc.nextDouble();
+//                Lanche lanche = new Lanche(descricao, preco, peso);
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+    }
+
+    public static void listarProduto() {
+
+    }
+
+    public static void removerProduto(Scanner sc) {
+
+    }
+
+    public static void editarProduto(Scanner sc) {
+
+    }
+
 }
